ac4480a01b76d10f420df53be70c4b70370405c9
David Oladapo <ibksmart2004@gmail.com>
2024-07-13 19:11:11 +0100

    Add files via upload

---
+++ fns_and_dsa/shopping_list_manager.py
@@ -0,0 +1,48 @@
+def display_menu():
+    print("\nShopping List Menu")
+    print("1. Add an item")
+    print("2. Remove an item")
+    print("3. View the list")
+    print("4. Exit")
+
+def add_item(shopping_list):
+    item = input("Enter the name of the item to add: ")
+    shopping_list.append(item)
+    print(f"'{item}' has been added to the shopping list.")
+
+def remove_item(shopping_list):
+    item = input("Enter the name of the item to remove: ")
+    if item in shopping_list:
+        shopping_list.remove(item)
+        print(f"'{item}' has been removed from the shopping list.")
+    else:
+        print(f"'{item}' is not in the shopping list.")
+
+def view_list(shopping_list):
+    if shopping_list:
+        print("\nCurrent Shopping List:")
+        for item in shopping_list:
+            print(f"- {item}")
+    else:
+        print("\nThe shopping list is empty.")
+
+def main():
+    shopping_list = []
+    while True:
+        display_menu()
+        choice = input("Choose an option (1-4): ")
+        
+        if choice == '1':
+            add_item(shopping_list)
+        elif choice == '2':
+            remove_item(shopping_list)
+        elif choice == '3':
+            view_list(shopping_list)
+        elif choice == '4':
+            print("Exiting the program. Goodbye!")
+            break
+        else:
+            print("Invalid choice. Please select a valid option from the menu.")
+
+if __name__ == "__main__":
+    main()
